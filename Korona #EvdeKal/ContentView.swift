48cff755b089d39efe0472809939f5459ce1409a
Halil YUCE <hllibrhmyc@gmail.com>
2020-03-20 17:18:48 +0300

    Initial Commit

---
+++ Korona #EvdeKal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Korona #EvdeKal
+//
+//  Created by Halil İbrahim YÜCE on 20.03.2020.
+//  Copyright © 2020 Halil İbrahim YÜCE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
